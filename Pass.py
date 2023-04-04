0dc2b7f785db22db76d96589da0ff6f1240f8f8e
VP-CYBER <129886537+VP-CYBER-404@users.noreply.github.com>
2023-04-05 02:53:22 +0600

    Create Pass.py

---
+++ Pass.py
@@ -0,0 +1,13 @@
+import getpass
+
+attemps = 0
+
+while attemps<1234567890:
+  username = input('Enter Username: ')
+  password = input('Enter Password: ')
+  
+  if username =='vp' and password =='cyber'
+      print('Incarect please carect Typ')
+      attemps += 1
+      continue 
+  
